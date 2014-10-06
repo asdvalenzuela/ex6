4014411670552d8f7459fdd949ce29e15f354fe0
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 16:14:46 -0700

    Added main function to call our restaurant_ratings function.

--- sorted_data.py
+++ sorted_data.py
@@ -1,16 +1,22 @@
+def restaurant_ratings(filename):
+    
+    ratings = open(filename)
+    ratings_dict = {}
+    for line in ratings:
+        line = line.rstrip()
+        key_value_pair = line.split(":")
+        name = key_value_pair[0]
+        rating = int(key_value_pair[1])
+        ratings_dict[name] = rating
 
-ratings = open("scores.txt")
-ratings_as_dict = {}
-for line in ratings:
-    line = line.rstrip()
-    key_value_pair = line.split(":")
-    name = key_value_pair[0]
-    rating = int(key_value_pair[1])
-    ratings_as_dict[name] = rating
+    names = ratings_dict.keys()
+    names.sort()
+    for name in names:
+        print "Restaurant '%s' is rated %d." % (name, rating)
 
-names = ratings_as_dict.keys()
-names.sort()
-for name in names:
-    print "Restaurant '%s' is rated %d" % (name, rating)
+def main():
 
+    restaurant_ratings("score.txt")
 
+if __name__ == "__main__":
+    main()
