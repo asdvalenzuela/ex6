21fccf238aa491383ad32c9dda2cf2e253d4abc7
Hackbright Student <info@hackbrightacademy.com>
2014-10-07 11:27:52 -0700

    Added code for sorting by rating.

--- sorted_data.py
+++ sorted_data.py
@@ -9,14 +9,19 @@ def restaurant_ratings(filename):
         rating = int(key_value_pair[1])
         ratings_dict[name] = rating
 
-    names = ratings_dict.keys()
-    names.sort()
-    for name in names:
-        print "Restaurant '%s' is rated %d." % (name, rating)
+    #the following code will sort alphabetically by name
+    #names = ratings_dict.keys()
+    #names.sort()
+    #for name in names:
+     #   print "Restaurant '%s' is rated %d." % (name, rating)
+
+    #the following code will sort by numerical rating
+    for key, value in sorted(ratings_dict.items(), key = lambda restaurant: restaurant[1]):
+        print "Restaurant '%s' is rated %s." % (key, value)
 
 def main():
 
-    restaurant_ratings("score.txt")
+    restaurant_ratings("scores.txt")
 
 if __name__ == "__main__":
     main()
